243cc2cb9226a93dd7d02e41ec7409c31896dc9e
romaricfosso <romaricfosso6@gmail.com>
2019-10-11 16:34:47 +0200

    first test to commithelloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld{
+   public class void main(String[] args){
+     //prints "hello welcom to my first project in cloud computer"
+system.out.println("Hello !! ,welcom to my first projet to cloud computer");
+};;
+}
